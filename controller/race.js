6010eead25a453a3475e0d8cdf77478b5cf5ad15
Igor.Tkachuk <I.Tka4yk@gmail.com>
2019-07-12 20:50:37 +0300

    somr changes

--- controller/race.js
+++ controller/race.js
@@ -18,9 +18,10 @@ class Race {
     this.io.use(this.updSocketWithLogin);
     this.io.on('connection', this.newCompetitor.bind(this));
     this.currentTimer = this.newTimer(RaceConfig.TIMER_TYPE_SEQ[this.state.stat])
+    this.currentTimer.start();
 
     setInterval(() => {
-      console.log(this.state.stat);
+      console.log( 'Current stat of race', this.state.stat);
     }, ServerConfig.TIMER_TICK);
   }
 
@@ -44,12 +45,17 @@ class Race {
     socket.join(RaceConfig.ROOM_WAITING);
 
     socket.on("disconnect", this.leaveCompetitor.bind(this, socket));
+    socket.on('SCORE', this.processScore(socket));
   }
 
   leaveCompetitor(socket, reason) {
     this.state.allSockets.delete(socket);
     console.log(`${socket._id} socket disconnected by reason '${reason}'`);
     this.io.to(socket._room).emit(RaceConfig.MSG_COMPETITOR_LEAVE, { user: socket._id });
+  }
+
+  processScore(socket) {
+
   }
 
   //Фабрика
@@ -74,17 +80,29 @@ class Race {
     console.log('nextRaceState invoked');
     if(this.state.stat === RaceConfig.RACE_STAT_SEQ.length - 1) {
       this.state.stat = RaceConfig.RACE_STAT_WAITING_FOR_START;
+    } else {
+      this.state.stat = RaceConfig.RACE_STAT_SEQ[this.state.stat + 1];
     }
-    this.state.stat = RaceConfig.RACE_STAT_SEQ[this.state.stat + 1];
 
     if(this.state.stat == RaceConfig.RACE_STAT_IN_PROGRESS) {
       //Перевести все сокеты (this.state.allSockets) в комнату ROOM_RACING
-      //socket._room, socket._id
+      this.state.allSockets.forEach(socket => {
+        socket.leave(RaceConfig.ROOM_WAITING);
+        socket.join(RaceConfig.ROOM_RACING);
+      });
+    }
+
+    if(this.state.stat == RaceConfig.RACE_STAT_WAITING_FOR_START) {
+      //Перевести все сокеты (this.state.allSockets) в комнату ROOM_WAITING
+      this.state.allSockets.forEach(socket => {
+        socket.leave(RaceConfig.ROOM_RACING);
+        socket.join(RaceConfig.ROOM_WAITING);
+      });
     }
 
     this.currentTimer = this.newTimer(RaceConfig.TIMER_TYPE_SEQ[this.state.stat]);
+    this.currentTimer.start();
   }
-
 }
 
 module.exports = Race;
--- controller/timer.js
+++ controller/timer.js
@@ -28,10 +28,10 @@ class Timer {
     this.competitorsList = competitorsList;
   }
 
-  // start() {
-  //   const fn = wrapper.call(this, this.payload.bind(this), this.changeStatFn);
-  //   this.interval = setInterval(fn, this.tick);
-  // }
+  start() {
+    const fn = wrapper.call(this, this.payload.bind(this), this.changeStatFn);
+    this.interval = setInterval(fn, this.tick);
+  }
 }
 
 class TimerRaceWaiting extends Timer{
@@ -39,19 +39,14 @@ class TimerRaceWaiting extends Timer{
     super(conf);
 
     this.limit = RaceConfig.BEFORE_RACE_TIME_AMMOUNT;
-    this.room = RaceConfig.ROOM_WAITING;
   }
 
   payload(time){
-    this.io.to(this.room).emit(RaceConfig.MSG_STAT_WAITING_FOR_START, {
+    this.io.to(RaceConfig.ROOM_WAITING).emit(RaceConfig.MSG_STAT_WAITING_FOR_START, {
+      stateForYou: 'wait',
       time,
       competitors: Array.from(this.competitorsList).map(socket => { return socket._id})
     });
-  }
-
-  start() {
-    const fn = wrapper.call(this, this.payload.bind(this), this.changeStatFn);
-    this.interval = setInterval(fn, this.tick);
   }
 }
 
@@ -60,11 +55,17 @@ class TimerInProcess extends Timer{
     super(conf);
 
     this.limit = RaceConfig.RACE_TIME_LIMIT;
-    this.room = RaceConfig.ROOM_RACING;
   }
 
   payload(time){
-    this.io.to(this.room).emit(RaceConfig.MSG_STAT_IN_PROGRESS, {
+    this.io.to(RaceConfig.ROOM_RACING).emit(RaceConfig.MSG_STAT_IN_PROGRESS, {
+      stateForYou: 'run',
+      time,
+      competitors: Array.from(this.competitorsList).map(socket => { return socket._id})
+    });
+
+    this.io.to(RaceConfig.ROOM_WAITING).emit(RaceConfig.MSG_STAT_IN_PROGRESS, {
+      stateForYou: 'wait',
       time,
       competitors: Array.from(this.competitorsList).map(socket => { return socket._id})
     });
@@ -76,11 +77,17 @@ class TimerRewarding extends Timer{
     super(conf);
 
     this.limit = RaceConfig.TIME_FOR_REWARDING;
-    this.room = RaceConfig.ROOM_RACING;
   }
 
   payload(time){
-    this.io.to(this.room).emit(RaceConfig.MSG_STAT_REWARDING, {
+    this.io.to(RaceConfig.ROOM_RACING).emit(RaceConfig.MSG_STAT_REWARDING, {
+      stateForYou: 'run',
+      time,
+      competitors: Array.from(this.competitorsList).map(socket => { return socket._id})
+    });
+
+    this.io.to(RaceConfig.ROOM_WAITING).emit(RaceConfig.MSG_STAT_REWARDING, {
+      stateForYou: 'wait',
       time,
       competitors: Array.from(this.competitorsList).map(socket => { return socket._id})
     });
